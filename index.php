4e7594f4e5f6c85be057ccd4163d7bec5137c64c
gonz <gonzamoiguer@gmail.com>
2020-08-03 23:23:20 -0300

    initial. vandando

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+require './medoo/src/Medoo.php';
+use Medoo\Medoo;
+
+if(isset($_GET["coso"])){
+     include 'config.php';
+    // Initialize
+    $database = new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => $config["dbname"],
+        'server'        => 'localhost',
+        'username'      => $config["dbuser"],
+        'password'      => $config["dbpass"]
+    ]);
+
+    $hash = md5($config["sec"]);
+    $time = date("Y-m-d H:i:s", time());
+
+    $database->insert('cursores', [
+        'pos'   => $_GET["pos"],
+        'time'  => $time
+    ]);
+}
+
+
+ ?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+
+    <title>objeto-b</title>
+    <link rel="icon" type="image/png" href="">
+
+    <meta name="description" content="">
+    <meta name="author" content="OBJETO-B">
+
+
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@700&display=swap" rel="stylesheet">
+    <link href='./style.css?v=1' rel='stylesheet' />
+</head>
+<body>
+
+
+<div id="app" :class="{fullscreen: !modalClosed}" v-cloak>
+    <div id="intro" class="">
+        <h1>{{titulo}}</h1>
+    </div>
+    <div class="desc">
+        <p>{{introTxt}}</p>
+    </div>
+    <div class="films">
+        <div class="item" v-for="vid in vids" v-bind:style="{ backgroundImage: `url(${vid.thumb.url})` }" @click="OpenVideo(vid)">
+            <div class="inner">
+                <h2 class="title">{{vid.titulo}}</h2>
+            </div>
+    	</div>
+
+
+    </div>
+
+
+
+
+
+    <div id="modal" :class="{closed: modalClosed}" v-if="typeof openedVid !== 'undefined'">
+            <div class="columns">
+                <div class="column videoContainer">
+                    <video ref="videoPlayer" width="450" controls v-bind:src="openedVid.video.url" autoplay></video>
+                </div>
+                <div class="column videoMetadata">
+                    <p><strong>{{openedVid.titulo}}</strong><p>
+                    <div v-if="openedVid.autor">por {{openedVid.autor}}</div>
+                    <div v-html="openedVid.texto.html"></div>
+                </div>
+            </div>
+            <div class="cerrar" @click="CloseVideo()"><span>[X]</span></div>
+
+
+    </div>
+
+
+
+</div><!-- app -->
+
+
+
+
+
+
+    <footer>
+
+    </footer>
+
+    <!-- md5 -->
+    <script src="./md5.min.js"></script>
+    <!-- superagent -->
+    <script src="https://cdn.jsdelivr.net/npm/superagent@5.3.1/dist/superagent.min.js" integrity="sha256-Z+shWFMYVbUNCQNUL1R4yVVi1rVDwLDBG3a0K93dQkc=" crossorigin="anonymous"></script>
+    <!-- vue -->
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+    <!-- main -->
+    <script src="./script.js"></script>
+</body>
---
+++ script.js
@@ -0,0 +1,155 @@
+(function() {
+    setTimeout(function(){
+        document.getElementById("intro").classList.add("opened")
+    },600)
+    const options = {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ query: `{ videos { id titulo video { url } thumb { url } texto { html } } }`})
+  };
+
+  fetch(`https://api-eu-central-1.graphcms.com/v2/ckdeyuw9er2q401xs12xcgnom/master`, options)
+      .then(res => res.json())
+      // .then(res => console.log(res.data.videos))
+      .then(res => app.SetVideos(res.data.videos))//app._data.vids = res.data.videos.slice(0,0) )
+})();
+
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        titulo: "objeto-b",
+        introTxt: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut eu sem integer vitae justo. Fringilla urna porttitor rhoncus dolor purus non. Sit amet cursus sit amet dictum sit amet. Aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc. Imperdiet nulla malesuada pellentesque elit eget gravida cum. Nunc mattis enim ut tellus elementum sagittis vitae et. Eget sit amet tellus cras adipiscing enim eu turpis egestas. Vivamus arcu felis bibendum ut tristique et egestas. Enim neque volutpat ac tincidunt. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Tellus in metus vulputate eu scelerisque felis. Nisl purus in mollis nunc sed.
+
+Egestas diam in arcu cursus euismod quis. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Odio euismod lacinia at quis risus sed. Venenatis a condimentum vitae sapien pellentesque. Eget magna fermentum iaculis eu non diam phasellus vestibulum lorem. Scelerisque eleifend donec pretium vulputate sapien nec sagittis. Nunc aliquet bibendum enim facilisis gravida neque. Mus mauris vitae ultricies leo integer malesuada. Morbi enim nunc faucibus a. Orci porta non pulvinar neque laoreet suspendisse. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Ut etiam sit amet nisl purus in.
+
+Neque sodales ut etiam sit amet nisl purus. A erat nam at lectus urna duis. Risus pretium quam vulputate dignissim suspendisse in est ante in. Sem nulla pharetra diam sit amet nisl suscipit. Blandit massa enim nec dui nunc mattis enim ut tellus. Vitae et leo duis ut diam. Tortor aliquam nulla facilisi cras fermentum odio. Vulputate mi sit amet mauris commodo quis. Donec ultrices tincidunt arcu non sodales neque. Quam adipiscing vitae proin sagittis nisl. Sollicitudin aliquam ultrices sagittis orci. Quisque egestas diam in arcu cursus euismod quis. Nam libero justo laoreet sit amet. Hac habitasse platea dictumst vestibulum rhoncus est. Auctor eu augue ut lectus arcu bibendum at varius. Leo integer malesuada nunc vel risus. Aliquet enim tortor at auctor urna nunc id.
+
+Sit amet facilisis magna etiam tempor orci eu. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Sed blandit libero volutpat sed. Feugiat in fermentum posuere urna nec tincidunt. Ultricies lacus sed turpis tincidunt id. Posuere ac ut consequat semper viverra. Odio aenean sed adipiscing diam donec adipiscing. Arcu non odio euismod lacinia at quis risus sed. Sem et tortor consequat id porta nibh venenatis cras sed. Blandit massa enim nec dui nunc. Interdum posuere lorem ipsum dolor. Diam donec adipiscing tristique risus nec feugiat in fermentum. Cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod. Ut tellus elementum sagittis vitae et leo duis ut diam.
+
+Placerat in egestas erat imperdiet sed euismod nisi porta lorem. Facilisis gravida neque convallis a cras semper. Velit euismod in pellentesque massa placerat. Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies. Duis at consectetur lorem donec massa sapien faucibus et. Nulla facilisi morbi tempus iaculis urna id. Turpis massa tincidunt dui ut. Ultrices neque ornare aenean euismod elementum. Integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Vel elit scelerisque mauris pellentesque pulvinar pellentesque. Sit amet volutpat consequat mauris nunc. Aenean sed adipiscing diam donec adipiscing tristique risus nec feugiat.`,
+        vids:[],
+        openedVid: {titulo:"", texto:{html:""}, thumb:{url:""}, video   :{url:""}},
+        modalClosed: true
+    },
+
+
+    methods:{
+        SetVideos: function (newVids) {
+            this.vids = newVids
+        },
+        OpenVideo: function(ele){
+            this.openedVid = ele;
+            this.modalClosed = false;
+            this.$refs.videoPlayer.play()
+        },
+        CloseVideo: function(){
+            this.modalClosed = true;
+            this.$refs.videoPlayer.pause()
+        }
+    }
+})
+
+
+
+
+var stop = false;
+var frameCount = 0;
+var fps, fpsInterval, startTime, now, then, elapsed;
+
+startAnimating(2);
+// initialize the timer variables and start the animation
+function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
+
+
+function animate() {
+    // request another frame
+    requestAnimationFrame(animate);
+    // calc elapsed time since last loop
+    now = Date.now();
+    elapsed = now - then;
+    // if enough time has elapsed, draw the next frame
+    if (elapsed > fpsInterval) {
+
+        // Get ready for next frame by setting then=now, but also adjust for your
+        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+        then = now - (elapsed % fpsInterval);
+
+        // Put your drawing code here
+        tick();
+    }
+}
+
+
+
+var xpos;
+var ypos;
+function findScreenCoords(mouseEvent)
+{
+  if (mouseEvent)
+  {
+    //FireFox
+    xpos = mouseEvent.screenX;
+    ypos = mouseEvent.screenY;
+  }
+  else
+  {
+    //IE
+    xpos = window.event.screenX;
+    ypos = window.event.screenY;
+  }
+}
+document.getElementsByTagName("body")[0].onmousemove = findScreenCoords;
+
+var cantPosiciones = 100;
+var posiciones = []
+var savedPosiciones = false;
+function tick(){
+    // console.log(xpos/window.innerWidth,ypos/window.innerHeight)
+    if(posiciones.length < cantPosiciones){
+        posiciones.push([xpos/window.innerWidth,ypos/window.innerHeight])
+    }else{
+        if(!savedPosiciones){
+            console.log(posiciones)
+            savedPosiciones = true;
+            // GUARDAR EN DB
+        }
+    }
+
+}
+
+// requestAnimationFrame polyfill
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+// MIT license
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
